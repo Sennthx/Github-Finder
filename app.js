563727e7fa298435f638a449df2e2287c21b6a26
Sennthx <totharon1173@gmail.com>
2022-12-14 10:30:22 +0100

    Added github api authentication

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-// Init
+// init
 const github = new Github;
 
-// userInput
-const userInput = document.querySelector("#user-input");
+const userSearch = document.querySelector("#user-search");
 
-// userInput event listener
-userInput.addEventListener("keyup", (e) => {
-    // Get inpu text
+// Adding event listener to user search input
+userSearch.addEventListener("keyup", (e) => {
     const userText = e.target.value;
 
     if(userText !== "") {
         // Make http call
         github.getUser(userText)
         .then(data => {
-            if(data.profile.message === "Not Found") {
-                // Show alert
-                
-            } else {
-                // Show profile
-            }
+        if(data.profile.message === "Not Found") {
+        }
+        else {
+            console.log(data);
+        }
         })
-    } else {
-        // Clear the profile
     }
-})
+    else {
+
+    }
+});
--- github.js
+++ github.js
@@ -1,12 +1,15 @@
 class Github {
     async getUser(user) {
-        
-        const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Basic ' + btoa('7aa5a917d0f12a7811d3:8f2525724dddfcd5538f203ffaaa648d068da57d')
+        }});
 
-        const profile = await profileResponse.json();
+        const profileData = await profileResponse.json();
 
         return {
-            profile
+            profile: profileData
         }
     }
 }
